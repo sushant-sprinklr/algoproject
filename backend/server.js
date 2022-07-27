d11486c450002130a49a43a1ec49f00e9a324b57
sushant-sprinklr <sushant.guha@sprinklr.com>
2022-07-27 13:32:13 -0700

    server initial setup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,16 @@
 const app = require("express")();
+require("dotenv").config;
+const { InfluxDB, Point } = require("@influxdata/influxdb-client");
+const token = process.env.INFLUXDB_TOKEN;
+const url = process.env.SERVER_URL;
+const client = new InfluxDB({ url, token });
+let org = process.env.ORG_NAME;
+let bucket = `testBucket`;
 
+runQry = async () => {
+  try {
+    //
+  } catch (err) {
+    console.log(`Error is: ${err}`);
+  }
+};
